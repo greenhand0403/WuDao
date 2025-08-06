229ffe4d8b9bc4eeb980002590e2f07c6e0a54f0
zengwangcheng <38219893+greenhand0403@users.noreply.github.com>
2025-08-06 22:40:28 +0800

    复制了日耀爆炸效果

--- Content/Projectiles/Ranged/SolarBulletProjectile.cs
+++ Content/Projectiles/Ranged/SolarBulletProjectile.cs
@@ -54,51 +54,56 @@ namespace WuDao.Content.Projectiles.Ranged
             // }
 
             // 获取反射方向
-            Vector2 normal = Vector2.Zero;
+            // Vector2 normal = Vector2.Zero;
 
-            if (Math.Abs(Projectile.velocity.X) > Math.Abs(Projectile.velocity.Y))
-            {
-                // 横向碰撞（左右墙）
-                normal = new Vector2(Math.Sign(Projectile.velocity.X), 0f);
-            }
-            else
-            {
-                // 纵向碰撞（上下地板或天花板）
-                normal = new Vector2(0f, Math.Sign(Projectile.velocity.Y));
-            }
+            // if (Math.Abs(Projectile.velocity.X) > Math.Abs(Projectile.velocity.Y))
+            // {
+            //     // 横向碰撞（左右墙）
+            //     normal = new Vector2(Math.Sign(Projectile.velocity.X), 0f);
+            // }
+            // else
+            // {
+            //     // 纵向碰撞（上下地板或天花板）
+            //     normal = new Vector2(0f, Math.Sign(Projectile.velocity.Y));
+            // }
 
-            // 反方向（爆炸应该往外喷）
-            Vector2 baseDir = -normal;
+            // // 反方向（爆炸应该往外喷）
+            // Vector2 baseDir = -normal;
 
-            for (int i = 0; i < 5; i++)
-            {
-                Vector2 randomOffset = new Vector2(Main.rand.NextFloat(-0.6f, 0.6f), Main.rand.NextFloat(-0.6f, 0.6f));
-                Vector2 dustVelocity = (baseDir + randomOffset).SafeNormalize(Vector2.UnitY) * Main.rand.NextFloat(10f, 20f);
+            // for (int i = 0; i < 5; i++)
+            // {
+            //     Vector2 randomOffset = new Vector2(Main.rand.NextFloat(-0.6f, 0.6f), Main.rand.NextFloat(-0.6f, 0.6f));
+            //     Vector2 dustVelocity = (baseDir + randomOffset).SafeNormalize(Vector2.UnitY) * Main.rand.NextFloat(10f, 20f);
 
-                Dust.NewDustPerfect(Projectile.Center, DustID.SolarFlare, dustVelocity, 150, default, 1.8f).noGravity = true;
-            }
+            //     Dust.NewDustPerfect(Projectile.Center, DustID.SolarFlare, dustVelocity, 150, default, 1.8f).noGravity = true;
+            // }
             return false;
         }
 
         public override void OnKill(int timeLeft)
         {
             SoundEngine.PlaySound(SoundID.Item10.WithVolumeScale(0.5f).WithPitchOffset(0.8f), Projectile.position);
-            for (var i = 0; i < 2; i++)
-            {
-                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.SolarFlare, 0.5f, 0.5f, 0, default, 1f);
-            }
+            // for (var i = 0; i < 2; i++)
+            // {
+            //     Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.SolarFlare, 0.5f, 0.5f, 0, default, 1f);
+            // }
             Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
 
-            // 生成一个SolarWhipSwordExplosion爆炸
-            Projectile.NewProjectile(
-                Projectile.GetSource_FromThis(),
-                Projectile.Center,
-                Vector2.One,
-                ProjectileID.SolarWhipSwordExplosion,
-                Projectile.damage,
-                Projectile.knockBack,
-                Projectile.owner
-            );
+            if (Main.myPlayer == Projectile.owner)
+            {
+                Projectile p2 = Main.projectile[Projectile.NewProjectile(
+                    Projectile.GetSource_FromThis(),
+                    Projectile.Center,
+                    Vector2.Zero,
+                    ProjectileID.DaybreakExplosion,
+                    Projectile.damage,
+                    Projectile.knockBack,
+                    Projectile.owner,
+                    0,
+                    0.85f + Main.rand.NextFloat() * 1.15f
+                )];
+                p2.CritChance = 0;
+            }
         }
 
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
